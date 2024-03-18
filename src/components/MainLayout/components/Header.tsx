1cff3869044a5580640974c8590c66a7e175c38e
Hayk_Margaryan1 <Hayk_Margaryan1@epam.com>
2024-03-18 12:25:51 +0400

    changed store header

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Store of Hayk!
           </Link>
         </Typography>
 
