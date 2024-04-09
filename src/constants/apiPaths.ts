b168d97a74dd04491f3295e70cf9ff4085a6de05
Hayk_Margaryan1 <Hayk_Margaryan1@epam.com>
2024-04-09 19:26:11 +0400

    updated API URL

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://ub91trpfva.execute-api.eu-central-1.amazonaws.com/dev",
+  product: "https://gyun3z11lb.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-central-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-central-1.amazonaws.com/dev",
