cb31b3192f2aa69590c2eded611bdfafc14011ee
Hayk_Margaryan1 <Hayk_Margaryan1@epam.com>
2024-04-21 12:08:16 +0400

    Integration with S3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://gyun3z11lb.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://qt3lpzaawh.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-central-1.amazonaws.com/dev",
 };
